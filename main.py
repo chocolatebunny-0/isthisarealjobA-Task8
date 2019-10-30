4cf5df3d18a58392dd5b74dbcc2c4ec86757f7c9
Toluwaleke <lekeariyo2015@gmail.com>
2019-10-30 16:20:20 +0100

    adjusted ocr for api integration

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 from flask import jsonify
 from emailverify import scrape
 from flask import request
+from datetime import datetime
+from werkzeug import secure_filename
+from OCR import convert_image_to_test
 
 
 app = Flask(__name__)
@@ -17,6 +21,15 @@ def index():
 @app.route('/ha')
 def indexa():
     return "loooo"
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files['file']
+    filename = os.path.join('files', secure_filename(str(datetime.now()) + '.jpg'))
+    f.save(filename)
+    return convert_image_to_test(filename)
+    
 
 
 if __name__ == "__main__":
