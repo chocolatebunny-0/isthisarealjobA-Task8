04d110446aab7c4fbb988e464a268a706318dd19
Toluwaleke <lekeariyo2015@gmail.com>
2019-10-31 00:44:08 +0100

    troubleshooting

--- coynamechecker.py
+++ coynamechecker.py
@@ -20,18 +20,18 @@ def match_address_name(name, address):
     x = r.json() 
     y = x['results']
 
-    try:
+    #try:
         # keep looping upto lenght of y 
-        for i in range(len(y)): 
-            # Print name and address of the query and coy_address == address:
-            coy_name = y[i]['name']
-            coy_address = y[i]['formatted_address']
-            #print(coy_name)
-            #print(coy_address) 
-        if (name.lower() in coy_name.lower()) and (address.lower() in coy_address.lower()):
-            return "They correlate, It is probably legit"
-        else:
-            return "It's probably fake"
-    except NameError:
-        return "wrong input format"
+    for i in range(len(y)):
+        # Print name and address of the query and coy_address == address:
+        coy_name = y[i]['name']
+        coy_address = y[i]['formatted_address']
+        #print(coy_name)
+        #print(coy_address) 
+    if (name.lower() in coy_name.lower()) and (address.lower() in coy_address.lower()):
+        return "They correlate, It is probably legit"
+    else:
+        return "It's probably fake"
+    #except NameError:
+     #   return "wrong input format"
 #print(match_address_name("appzone", "350 borno way"))  
