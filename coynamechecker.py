44ce673994a318aa0c0f558a0d9894b758702220
Leke-Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2019-10-31 18:07:10 +0100

    Update coynamechecker.py

--- coynamechecker.py
+++ coynamechecker.py
@@ -21,17 +21,17 @@ def match_address_name(qname, address):
     y = x['results']
     coy_nameq = "ss"
     coy_address = "gg"
-    try:
-            # keep looping upto lenght of y 
-        for i in range(len(y)): 
-            # Print name and address of the query and coy_address == address:
-            coy_nameq = y[i]['name'] 
-            coy_address = y[i]['formatted_address'] 
-            lola = coy_nameq
-        if (qname.lower() in lola.lower()) and (address.lower() in coy_address.lower()):
-            return "They correlate, It is probably legit"
-        else:
-            return "It's probably fake"
-    except NameError:
+
+        # keep looping upto lenght of y 
+    for i in range(len(y)): 
+        # Print name and address of the query and coy_address == address:
+        coy_nameq = y[i]['name'] 
+        coy_address = y[i]['formatted_address'] 
+        lola = coy_nameq
+    if (qname.lower() in lola.lower()) and (address.lower() in coy_address.lower()):
+        return "They correlate, It is probably legit"
+    else:
         return "It's probably fake"
+    #except NameError:
+        #return "It's probably fake"
 #print(match_address_name("appzone", "350 borno way"))  
