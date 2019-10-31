9dbca79800efe20d86f697a8ad3dc719d547b10c
Toluwaleke <lekeariyo2015@gmail.com>
2019-10-31 01:39:37 +0100

    implemented api for name address match

--- coynamechecker.py
+++ coynamechecker.py
@@ -20,17 +20,17 @@ def match_address_name(qname, address):
     x = r.json() 
     y = x['results']
 
-    #try:
-        # keep looping upto lenght of y 
-    for i in range(len(y)): 
-        # Print name and address of the query and coy_address == address:
-        coy_nameq = y[i]['name'] 
-        coy_address = y[i]['formatted_address'] 
-        lola = coy_nameq
-    if (qname.lower() in lola.lower()) and (address.lower() in coy_address.lower()):
-        return "They correlate, It is probably legit"
-    else:
+    try:
+            # keep looping upto lenght of y 
+        for i in range(len(y)): 
+            # Print name and address of the query and coy_address == address:
+            coy_nameq = y[i]['name'] 
+            coy_address = y[i]['formatted_address'] 
+            lola = coy_nameq
+        if (qname.lower() in lola.lower()) and (address.lower() in coy_address.lower()):
+            return "They correlate, It is probably legit"
+        else:
+            return "It's probably fake"
+    except NameError:
         return "It's probably fake"
-    #except NameError:
-     #   return "wrong input format"
 #print(match_address_name("appzone", "350 borno way"))  
