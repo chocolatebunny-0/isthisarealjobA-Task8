b21fa795852c41ad34c97ea861b7e8ed0e278f2a
Toluwaleke <lekeariyo2015@gmail.com>
2019-10-31 01:03:17 +0100

    troubleshooting

--- coynamechecker.py
+++ coynamechecker.py
@@ -5,7 +5,7 @@ Created by Bilesanmi Faruk
 import requests, json 
   
 # enter your api key here 
-def match_address_name(name, address):
+def match_address_name(qname, address):
     api_key = 'AIzaSyDeqniyBHyf4XdEwOBAtvtI8xPWnvQE5pk'
 
     url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
@@ -13,7 +13,7 @@ def match_address_name(name, address):
     #name = input("Enter the name if the company: ")
     #address = input("Enter the address of the company:  ")
 
-    query = name
+    query = qname
 
     r = requests.get(url + 'query=' + query + '&key=' + api_key)
     #  json format data into python format data 
@@ -22,13 +22,13 @@ def match_address_name(name, address):
 
     #try:
         # keep looping upto lenght of y 
-    for i in range(len(y)):
+    for i in range(len(y)): 
         # Print name and address of the query and coy_address == address:
         coy_name = y[i]['name']
         coy_address = y[i]['formatted_address']
         #print(coy_name)
         #print(coy_address) 
-    if (name.lower() in coy_name.lower()) and (address.lower() in coy_address.lower()):
+    if (qname.lower() in coy_name.lower()) and (address.lower() in coy_address.lower()):
         return "They correlate, It is probably legit"
     else:
         return "It's probably fake"
