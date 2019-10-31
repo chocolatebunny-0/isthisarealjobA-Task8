89ca1ce107c5eaa085c635684040b1608924f081
Anwana John <jayanwana@gmail.com>
2019-10-31 09:15:16 +0100

    update

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,27 +1,26 @@
-import joblib
-
-
 def extract_features(word_list):
     return dict([(word, True) for word in word_list])
 
 
-def get_predictions(input_text, classifier):
+def get_predictions(posts, classifier):
     """
     Generate sentiment analysis of a given text
-    :param input_text: str;
+    :param posts: list; list of posts scraped from nairaland
     :param classifier: model
     :return: predicted sentiment, positive or negative
     """
-    prob_dist = classifier.prob_classify(extract_features(input_text.split()))
-    pred_sentiment = prob_dist.max()
-    print("Predicted sentiment:", pred_sentiment)
-    print("Probability:", round(prob_dist.prob(pred_sentiment), 2))
-    return str(pred_sentiment)+" "+str(round(prob_dist.prob(pred_sentiment), 2))
+    predictions = []
+    for text in posts:
+        features = dict([(word, True) for word in text.split()])
+        probability_dist = classifier.prob_classify(features)
+        prediction_sentiment = probability_dist.max()
+        if prediction_sentiment == 'Positive':
+            predictions.append(1)
+        else:
+            predictions.append(0)
+    avg_prediction = round(sum(predictions) / len(predictions))
+    if avg_prediction == 1:
+        return "Positive"
+    else:
+        return 'Negative'
 
-
-filename = "data/classifier.sav"
-loaded_class = joblib.load(filename)
-
-#text = 'very bad company, did not pay us on time,' \
-#       ' very hostile environment, didnt enjoy my time there one bit, terrible experience'
-get_predictions(text, loaded_class)
