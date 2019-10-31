68f2f9b0632803e21356f216f1dafeef03850ca3
Toluwaleke <lekeariyo2015@gmail.com>
2019-10-31 08:29:27 +0100

    api for name verification on CAC

--- cac_leke.py
+++ cac_leke.py
@@ -16,11 +16,10 @@ import os
 
 def name_verification(company_name):
     options = webdriver.ChromeOptions()
-    options.add_argument('--disable-notifications')
-    options.add_argument("--headless")
-    options = webdriver.ChromeOptions()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BINARY")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--disable-notifications")
     options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(chrome_options=options)
     browser.get("https://publicsearch.cac.gov.ng/")
