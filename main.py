bc0b6fe160fe4e751c19018b59da990617528d94
Bilesanmi farouk <bilesanmifaruk@gmail.com>
2019-11-05 23:21:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    email = request.data('email', None)
+    email = request.form.get('email', None)
     if email:
         return jsonify(Email=scrape(email))
     return jsonify({'status': 'No email supplied'})
