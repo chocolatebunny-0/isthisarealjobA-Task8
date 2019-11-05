2291f420484f1740ce02faf195cb599bfa0c47cb
Bilesanmi farouk <bilesanmifaruk@gmail.com>
2019-11-05 18:13:37 +0100

    changed request.args.get to request.form

--- main.py
+++ main.py
@@ -15,53 +15,52 @@ from nairaland_senti import*
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    email = request.args.get('email', None)
+    email = request.form('email', None)
     if email:
         return jsonify(Email=scrape(email))
     return jsonify({'status': 'No email supplied'})
 
 @app.route('/error_count')
 def error_count():
-    msg_body = request.args.get('msg_body', None)
+    msg_body = request.form('msg_body', None)
     if msg_body:
         return jsonify(Error_Counter=error_counter(msg_body))
     return jsonify({'status': 'No message body supplied'})
 
 @app.route('/check_address')
 def check_address():
-    address = request.args.get('address', None)
+    address = request.form('address', None)
     if address:
         return jsonify(Check_Address=address_checker(address))
     return jsonify({'status': 'No address supplied'})
 
 @app.route('/match_name_and_address')
 def match_name_and_address():
-    thename = request.args.get('name', None)
-    theaddress = request.args.get('address', None)
+    thename = request.form('name', None)
+    theaddress = request.form('address', None)
     if theaddress:
         return jsonify(Match_Name_Address=match_address_name(thename, theaddress))
     return jsonify({'status': 'Name or address not supplied'})
 
 @app.route('/check_name')
 def check_name():
-    check = request.args.get('company_name', None)
+    check = request.form('company_name', None)
     if check:
         return jsonify(Verify_Name=name_verification(check))
     return jsonify({'status': 'Name not correctly supplied'})
 
 @app.route('/linkedin_presence')
 def linkedin_presence():
-    presence = request.args.get('ln_company_name', None)
+    presence = request.form('ln_company_name', None)
     if presence:
         return jsonify(Verify_on_Linkedin=linkedin_job_search(presence))
     return jsonify({'status': 'Name not correctly supplied'})
 
 @app.route('/nairaland_sentimental_analysis')
 def nairaland_sentimental_analysis():
-    n_analysis = request.args.get('searchTerm', None)
+    n_analysis = request.form('searchTerm', None)
     if n_analysis:
         return jsonify(Verify_on_Nairaland=NScraper(n_analysis))
     return jsonify({'status': 'Name not correctly supplied'})
@@ -76,12 +75,130 @@ def upload():
 @app.route('/full')
 def check_job():
     
+    coy_name = request.form('coy_name', None)
+    address = request.form('address', None)
+    msg_body = request.form('body', None)
+    email = request.form('email', None)
+    
+    global confidence
+    confidence = 0
+    
+    if "is OK" in scrape(email):
+        confidence += 10
+    else:
+        confidence = confidence
+    
+    print(confidence, " done")
+    
+    if "real" in error_counter(msg_body):  
+        confidence += 15
+    else:
+        confidence = confidence
+        
+    print(confidence, " done") 
+    
+    if address_checker(address) == "available":
+        confidence += 10
+    else:
+        confidence = confidence
+
+    print(confidence, " done")
+
+    if linkedin_job_search(coy_name) == "The company is on Linkedin and analysis shows it is a big company":
+        confidence += 20
+    else:
+        confidence = confidence
+
+    print(confidence, " done")
+
+    if "be real" in NScraper(coy_name):
+        confidence += 15
+    elif "conclude on" in NScraper(coy_name):
+        confidence += 7
+    else:
+        confidence = confidence
+
+    print(confidence, " done")
+
+    if name_verification(coy_name) == "company records are found on CAC page":
+        confidence += 30
+    else:
+        confidence = 0
+
+    print(confidence, " done")
+
+    return jsonify(Confidence=str(confidence)+"%")
+
+
+
+#backup
+
+@app.route('/1')
+def index():
+    email = request.args.get('email', None)
+    if email:
+        return jsonify(Email=scrape(email))
+    return jsonify({'status': 'No email supplied'})
+
+@app.route('/error_count1')
+def error_count():
+    msg_body = request.args.get('msg_body', None)
+    if msg_body:
+        return jsonify(Error_Counter=error_counter(msg_body))
+    return jsonify({'status': 'No message body supplied'})
+
+@app.route('/check_address1')
+def check_address():
+    address = request.args.get('address', None)
+    if address:
+        return jsonify(Check_Address=address_checker(address))
+    return jsonify({'status': 'No address supplied'})
+
+@app.route('/match_name_and_address1')
+def match_name_and_address():
+    thename = request.args.get('name', None)
+    theaddress = request.args.get('address', None)
+    if theaddress:
+        return jsonify(Match_Name_Address=match_address_name(thename, theaddress))
+    return jsonify({'status': 'Name or address not supplied'})
+
+@app.route('/check_name1')
+def check_name():
+    check = request.args.get('company_name', None)
+    if check:
+        return jsonify(Verify_Name=name_verification(check))
+    return jsonify({'status': 'Name not correctly supplied'})
+
+@app.route('/linkedin_presence1')
+def linkedin_presence():
+    presence = request.args.get('ln_company_name', None)
+    if presence:
+        return jsonify(Verify_on_Linkedin=linkedin_job_search(presence))
+    return jsonify({'status': 'Name not correctly supplied'})
+
+@app.route('/nairaland_sentimental_analysis1')
+def nairaland_sentimental_analysis():
+    n_analysis = request.args.get('searchTerm', None)
+    if n_analysis:
+        return jsonify(Verify_on_Nairaland=NScraper(n_analysis))
+    return jsonify({'status': 'Name not correctly supplied'})
+
+@app.route('/upload1', methods=['POST'])
+def upload():
+    f = request.files['file']
+    filename = os.path.join('files', secure_filename(str(datetime.now()) + '.jpg'))
+    f.save(filename)
+    return convert_image_to_test(filename)
+
+@app.route('/full1')
+def check_job():
+    
     coy_name = request.args.get('coy_name', None)
     address = request.args.get('address', None)
     msg_body = request.args.get('body', None)
     email = request.args.get('email', None)
     
-    global confidence
+    #global confidence
     confidence = 0
     
     if "is OK" in scrape(email):
